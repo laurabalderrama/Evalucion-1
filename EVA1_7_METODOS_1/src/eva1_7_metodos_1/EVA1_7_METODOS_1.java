b7ebffc9e52fb3b3594f24760bb4fa2319ceb8ea
laurabalderrama <124391755+laurabalderrama@users.noreply.github.com>
2023-02-21 12:51:58 -0600

    Add files via upload

---
+++ EVA1_7_METODOS_1/src/eva1_7_metodos_1/EVA1_7_METODOS_1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_7_metodos_1;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_7_METODOS_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    myMethod();   
+    }
+    public static void myMethod(){
+        System.out.println("you have called me! my name is myMethood");
+    }
+}
