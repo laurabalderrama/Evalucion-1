2cc43ce11c5b5721973e6c5a5219b07c92536be0
laurabalderrama <124391755+laurabalderrama@users.noreply.github.com>
2023-02-23 12:46:10 -0600

    Add files via upload

---
+++ EVA1_13_ALCANCE_2/src/eva1_13_alcance_2/EVA1_13_ALCANCE_2.java
@@ -0,0 +1,29 @@
+
+
+package eva1_13_alcance_2;
+
+/**
+ *
+ * LAURA BALDERRAMA
+ */
+public class EVA1_13_ALCANCE_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            System.out.println(x);
+            int x = 100;
+            System.out.println(x);
+            {
+                int y = 1000;
+                System.out.println(y);
+            }
+            System.out.println(y);
+        }
+        System.out.println(i);
+        System.out.println(x);
+    } 
+}
