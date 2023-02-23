ae756b0702807c4cf14b12e4f17479c7ab6a1476
laurabalderrama <124391755+laurabalderrama@users.noreply.github.com>
2023-02-23 12:45:44 -0600

    Add files via upload

---
+++ EVA1_12_ALCANCE/src/eva1_12_alcance/EVA1_12_ALCANCE.java
@@ -0,0 +1,18 @@
+
+package eva1_12_alcance;
+
+/**
+ *
+ * LAURA BALDERRAMA
+ */
+public class EVA1_12_ALCANCE {
+
+   
+    public static void main(String[] args) {
+        System.out.println(x);//no esxiste porque esta declarada después de la instruccción
+        System.out.println("args");// args esta declarado antes de la función
+        int x = 100;
+        System.out.println(x);
+    }
+    
+}
