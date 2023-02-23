43507ff6c015a738eedf64f32a614ae9331260af
laurabalderrama <124391755+laurabalderrama@users.noreply.github.com>
2023-02-23 12:46:56 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,25 @@
+
+package eva1_15_sobrecarga;
+
+/**
+ *
+ * LAURA BALDERRAMA
+ */
+public class EVA1_15_SOBRECARGA {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello world!!");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+        
+    }
+    public static void algo(){
+        
+    }
+    public static void algo(int x){
+        
+    }
+}
